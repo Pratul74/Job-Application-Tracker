ca5a994cee6250ab995e17317b6b132171a1c8e3
Pratul Kumar <pratulkumar2003@gmail.com>
2026-05-10 14:41:49 +0530

    Fix user form error

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -3,30 +3,94 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User
 
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model=User
-        fields=('email', 'first_name', 'last_name', 'phone_number', 'linkedin_link', 'github_link', 'is_staff')
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'linkedin_link',
+            'github_link',
+            'is_staff'
+        )
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model=User
-        fields=('email', 'first_name', 'last_name', 'phone_number', 'linkedin_link', 'github_link', 'is_staff')
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'phone_number',
+            'linkedin_link',
+            'github_link',
+            'is_staff'
+        )
 
 
 @admin.register(User)
-class CustomeUserAdmin(UserAdmin):
-    form=CustomUserChangeForm
-    add_form=CustomUserCreationForm
-    list_display=('email', 'first_name', 'last_name', 'is_staff',)
-    list_filter=('is_staff','is_active')
-    fieldsets=((None, {"fields": ('email', 'first_name', 'last_name', 'phone_number', 'linkedin_link', 'github_link')}),
-               ("Permissions", {"fields":('is_staff', 'is_active', 'is_superuser')}))
-    add_fieldsets=(("Personal Info", 
-                    {"classes": ("wide",)},
-                    {"fields": ("email",  "first_name", "last_name", "phone_number", "linkedin_link", "github_link")}),
-                    ("Permissions", {"classes": ("wide",)}, {"fields": ("is_staff", "is_active", "is_superuser")}))
-    search_fields=('email',)
-    ordering=('email',)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_active')
+
+    filter_horizontal=('groups', 'user_permissions')
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'phone_number',
+                    'linkedin_link',
+                    'github_link'
+                )
+            }
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ('is_staff', 'is_active')
+            }
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            "Personal Info",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                    "linkedin_link",
+                    "github_link",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "classes": ("wide",),
+                "fields": ("is_staff", "is_active"),
+            },
+        ),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
 
 
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 
 class CustomUserManager(UserManager):
     def create_user(self, email, password = None, **extra_fields):
@@ -17,10 +17,10 @@ class CustomUserManager(UserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email=models.EmailField(unique=True)
-    first_name=models.CharField(max_length=100, blank=False)
-    last_name=models.CharField(max_length=100, blank=False)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100, null=True)
     phone_number=models.CharField(max_length=20, blank=True)
     linkedin_link=models.URLField(blank=True)
     github_link=models.URLField(blank=True)
